edaa6f5f32eb26984590f10dcdee2ca326de664b
hermlon <dev.hermlon@yamx.net>
2018-03-06 22:43:24 +0100

    Added exception handling

--- examples/delete_low_score.py
+++ examples/delete_low_score.py
@@ -12,9 +12,14 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-low_scores = session.query(NNTraining).filter(NNTraining.score < 50.0).all()
+low_scores = session.query(NNTraining).filter(NNTraining.score < 80.0).all()
 
 for low_score in low_scores:
     print('Removing %s with score %.2f%%' % (low_score.nn_saved_name, low_score.score))
     file = os.path.join(NN_MODEL_DIR, low_score.nn_saved_name)
-    os.remove(file)
+    session.delete(low_score)
+    try:
+        os.remove(file)
+    except FileNotFoundError:
+        print('Did not find on disk: %s' % file)
+session.commit()
