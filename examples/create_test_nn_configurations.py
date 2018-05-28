daeda5e343acae2e46647c32420e7baf5339cbfb
hermlon <dev.hermlon@yamx.net>
2018-05-28 17:19:29 +0200

    Cleaned up test NNConfiguration creator

--- examples/create_test_nn_configurations.py
+++ examples/create_test_nn_configurations.py
@@ -12,17 +12,11 @@ env = NNResEnv(NN_EMOT_DB, NN_MODEL_DIR, NN_EMOT_IMG_DIR)
 
 blocksize = 25
 bias = True
-"""
+
 for activation_function in [SigmoidActivationFunction, ReLuActivationFunction]:
     for cost_function in [LinearCostFunction, QuadraticCostFunction]:
         for learningrate in [0.1, 0.2, 0.3, 0.5, 0.7, 0.9, 1.5, 2.0, 3.0]:
             for layersizes in [(944, 300, 200, 150, 80, 7), (944, 500, 80, 7), (944, 700, 600, 300, 200, 80, 35, 7), (944, 150, 30, 7)]:
                 env.db.add(NNConfiguration(blocksize=blocksize, bias=bias, activation_function=activation_function, cost_function=cost_function, learningrate=learningrate, layersizes=layersizes))
 
-#env.db.commit()
-"""
-
-n = NNConfiguration(blocksize=blocksize, bias=bias, activation_function=SigmoidActivationFunction,
-                                cost_function=LinearActivationFunction, learningrate=0.2, layersizes=(944, 500, 80, 7))
-env.db.add(n)
 env.db.commit()
