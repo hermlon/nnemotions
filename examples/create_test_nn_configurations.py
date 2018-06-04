1e37c92e8cc6ad9031cf9e90f5f0fb63217eff83
hermlon <dev.hermlon@yamx.net>
2018-06-04 20:35:07 +0200

    added all emotions test

--- examples/create_test_nn_configurations.py
+++ examples/create_test_nn_configurations.py
@@ -6,11 +6,12 @@ from nnemotions.network.nn_functions import *
 
 blocksize = 10
 bias = True
+activation_function = SigmoidActivationFunction
+cost_function = QuadraticCostFunction
 
-for activation_function in [SigmoidActivationFunction]:
-    for cost_function in [QuadraticCostFunction]:
-        for learningrate in [0.05, 0.1, 0.15, 0.2, 0.3, 0.5, 0.7, 0.9, 1.5, 2.0, 3.0]:
-            for layersizes in [(5900, 2000, 500, 100, 30, 7), (5900, 500, 30, 7), (5900, 2000, 7)]:
-                env.db.add(NNConfiguration(blocksize=blocksize, bias=bias, activation_function=activation_function, cost_function=cost_function, learningrate=learningrate, layersizes=layersizes))
+for learningrate in [0.05, 0.1, 0.15, 0.2, 0.3, 0.5, 0.7, 0.9, 1.5, 2.0, 3.0]:
+    for layersizes in [(5900, 2000, 500, 100, 30, 7), (5900, 500, 30, 7), (5900, 2000, 7)]:
+        env.db.add(NNConfiguration(blocksize=blocksize, bias=bias, activation_function=activation_function,
+                                   cost_function=cost_function, learningrate=learningrate, layersizes=layersizes))
 
 env.db.commit()
