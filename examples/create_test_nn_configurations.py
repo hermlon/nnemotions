6d73444f6d42585bf2a516a874761d2acf59f093
hermlon <dev.hermlon@yamx.net>
2018-06-04 22:49:20 +0200

    new nn test configs

--- examples/create_test_nn_configurations.py
+++ examples/create_test_nn_configurations.py
@@ -4,13 +4,13 @@ from nnemotions.network.nn_functions import *
 
 # Generate some configurations for testing
 
-blocksize = 10
+blocksize = 25
 bias = True
 activation_function = SigmoidActivationFunction
 cost_function = QuadraticCostFunction
 
 for learningrate in [0.05, 0.1, 0.15, 0.2, 0.3, 0.5, 0.7, 0.9, 1.5, 2.0, 3.0]:
-    for layersizes in [(5900, 2000, 500, 100, 30, 7), (5900, 500, 30, 7), (5900, 2000, 7)]:
+    for layersizes in [(944, 500, 100, 30, 7), (944, 500, 30, 7), (944, 350, 7)]:
         env.db.add(NNConfiguration(blocksize=blocksize, bias=bias, activation_function=activation_function,
                                    cost_function=cost_function, learningrate=learningrate, layersizes=layersizes))
 
--- examples/training_all.py
+++ examples/training_all.py
@@ -34,10 +34,10 @@ for nn_config in nn_configs:
     trainingHelper = NNTrainingHelper(env, nn_config)
     trainingHelper.new_session()
     try:
-        for i in range(7):
+        for i in range(4):
             trainingHelper.train(img_train, emotions)
             print('epoch {}'.format(i))
     except(KeyboardInterrupt):
         print('Cancelling Training')
     trainingHelper.test(img_test, emotions)
-    trainingHelper.save_network('7 epochs; training all emotions all databases, first 150 for training, rest for testing', minscore=80)
+    trainingHelper.save_network('4 epochs; training all emotions all databases, first 150 for training, rest for testing', minscore=80)
